6eb3285aca7758300d93ac455d012dd20700cc80
Your Name <you@example.com>
2026-05-06 22:39:40 +0300

    Allow generated social images in robots

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,7 +6,7 @@ export default function robots(): MetadataRoute.Robots {
             {
                 userAgent: '*',
                 allow: '/',
-                disallow: ['/api/', '/*/opengraph-image*', '/*/twitter-image*'],
+                disallow: ['/api/'],
             },
             {
                 userAgent: 'Mediapartners-Google',
